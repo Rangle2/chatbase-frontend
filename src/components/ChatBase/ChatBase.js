6c21d5c5e8f642bc4c54f7029c1ef11909ff9dfe
Rangle2 <jhingood123@gmail.com>
2024-02-03 04:48:05 +0300

    style: Add more icons, contacts and chat

--- src/components/ChatBase/ChatBase.js
+++ src/components/ChatBase/ChatBase.js
@@ -22,6 +22,7 @@ const ChatBase = () => {
       <img className='Add-Friend' width="40" height="40" src="https://img.icons8.com/ios-glyphs/30/FFFFFF/add--v1.png" alt="add--v1"/>
       <img className='User-Settings' onClick={handleSignout} width="40" height="40" src="https://img.icons8.com/ios-filled/50/FFFFFF/settings.png" alt="settings"/>
       <div className='Friend-List-Bg'></div>
+
       <input
       className='Search-Textbox'
       id='search'
@@ -29,12 +30,18 @@ const ChatBase = () => {
       type='textbox'
       ></input>
       <img className='Search-Icon' width="32" height="32" src="https://img.icons8.com/windows/32/FFFFFF/search--v1.png" alt="search--v1"/>
+
+
+      <div className='Contact-Bg'></div>
+      <img className='Contact-Profile' width="50" height="50" src="https://img.icons8.com/ios-filled/50/FFFFFF/cat-profile.png" alt="cat-profile"/>
+      <div className='Contact-Name'>Friend</div>
    
 
       
       <div className='Friend-Bg'></div>
       <img className='Friend-Profile' width="50" height="50" src="https://img.icons8.com/ios-filled/50/FFFFFF/cat-profile.png" alt="cat-profile"/>
       <label className='Friend-Profile-Name'>Friend</label>
+      <img className='Friend-Settings' width="50" height="50" src="https://img.icons8.com/ios-filled/50/FFFFFF/ellipsis.png" alt="ellipsis"/>
      
       <div className='Message-Bg'></div>
       <img className='Add-Photo-Btn' width="40" height="40" src="https://img.icons8.com/android/24/FFFFFF/plus.png" alt="plus"/>
@@ -49,6 +56,9 @@ const ChatBase = () => {
 
      <img className='Add-Smile-Btn' width="40" height="40" src="https://img.icons8.com/ios-filled/50/FFFFFF/fat-emoji.png" alt="fat-emoji"/>
      <img className='Send-Message-Btn'width="40" height="40" src="https://img.icons8.com/material-sharp/24/FFFFFF/sent.png" alt="sent"/>
+     <div className='Chat-Container'></div>
+     <span className='User-Msg-Container'></span>
+     <span className='Friend-Msg-Container'></span>
     </div>
   );
 }
