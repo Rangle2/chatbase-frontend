7eaca4e954c0666e519543832c799ecda2cdd852
Rangle2 <jhingood123@gmail.com>
2024-02-05 03:08:04 +0300

    Fix: conflict problems

--- src/components/ChatBase/ChatBase.js
+++ src/components/ChatBase/ChatBase.js
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-import React, {useState} from 'react';
-import "..//ChatBase/ChatBase.css";
-import { signOut } from "firebase/auth";
-import auth from "../Firebase/firebase-config";
-import ChatUI from './ChatUI';
-
-
-const ChatBase = () => {
-  const [userMessage, setUserMessage] = useState("");
-  const [messages, setMessages] = useState([]);
-  
-
-const handleSignout = async () => {
-  try {
-      await signOut(auth);
-      console.log("User Signed out");
-  } catch (error) {
-      console.error("Sign out Failed", error);
-  }
-};
-
-
-const handleSendMessage = () => {
-  if (userMessage.trim() !== "") {
-    
-    
-
-    // Clear the input field
-    setUserMessage("");
-  }
-};
-
-const handleInputChange = (event) => {
-  setUserMessage(event.target.value);
-};
-
-=======
 import React, { useState, useEffect } from 'react';
 import "./ChatBase.css";
 import { signOut } from "firebase/auth";
@@ -135,7 +97,6 @@ const ChatBase = () => {
    
     
   }, [user]);
->>>>>>> bf8f211 (feat: Added new friendship feature)
 
 
 
@@ -145,12 +106,6 @@ const ChatBase = () => {
 
       {/* USER SECTION */}
       <div className='User-Bg'></div>
-<<<<<<< HEAD
-      <img className='User-Profile' width="50" height="50" src="https://img.icons8.com/ios-filled/50/FFFFFF/cat-profile.png" alt="cat-profile"/>
-      <label className='User-Profile-Name'>User</label>
-      <img className='Add-Friend' width="40" height="40" src="https://img.icons8.com/ios-glyphs/30/FFFFFF/add--v1.png" alt="add--v1"/>
-      <img className='User-Settings' onClick={handleSignout} width="40" height="40" src="https://img.icons8.com/ios-filled/50/FFFFFF/settings.png" alt="settings"/>
-=======
       {user && user.photoURL ? (
         <img className='User-Profile' width="50" height="50" src={user.photoURL} alt="user-profile" />
       ) : (
@@ -159,47 +114,11 @@ const ChatBase = () => {
       <label className='User-Profile-Name'>{user ? user.displayName || 'User' : 'User'}</label>
       <img className='Add-Friend' onClick={handleAddFriend} width="40" height="40" src="https://img.icons8.com/ios-glyphs/30/FFFFFF/add--v1.png" alt="add--v1" />
       <img className='User-Settings' onClick={handleSignout} width="40" height="40" src="https://img.icons8.com/ios-filled/50/FFFFFF/settings.png" alt="settings" />
->>>>>>> bf8f211 (feat: Added new friendship feature)
 
 
       {/* FRIEND LIST SECTION */}
       <div className='Friend-List-Bg'></div>
       <input
-<<<<<<< HEAD
-      className='Search-Textbox'
-      id='search'
-      placeholder='Search'
-      type='textbox'
-      ></input>
-      <img className='Search-Icon' width="32" height="32" src="https://img.icons8.com/windows/32/FFFFFF/search--v1.png" alt="search--v1"/>
-
-      {/* CONTACT LIST SECTION */}
-      <div className='Contact-Bg'></div>
-      <img className='Contact-Profile' width="50" height="50" src="https://img.icons8.com/ios-filled/50/FFFFFF/cat-profile.png" alt="cat-profile"/>
-      <div className='Contact-Name'>Friend</div>
-   
-        
-      {/* FRIEND SECTION */}
-      <div className='Friend-Bg'></div>
-      <img className='Friend-Profile' width="50" height="50" src="https://img.icons8.com/ios-filled/50/FFFFFF/cat-profile.png" alt="cat-profile"/>
-      <label className='Friend-Profile-Name'>Friend</label>
-      <img className='Friend-Settings' width="50" height="50" src="https://img.icons8.com/ios-filled/50/FFFFFF/ellipsis.png" alt="ellipsis"/>
-
- 
-      {/* MESSAGE TEXTBOX SECTION */}
-      
-      
-
-     {/* MESSAGE SECTION */}
-      <ChatUI 
-      userMessage={userMessage}
-      handleSendMessage={handleSendMessage}
-      handleInputChange={handleInputChange} 
-      messages={messages}   // Pass the messages array to ChatUI
-      setMessages={setMessages} />
-     </div>
-  );
-=======
         className='Search-Textbox'
         id='search'
         placeholder='Search'
@@ -253,7 +172,6 @@ const ChatBase = () => {
     </div>
   );
 
->>>>>>> bf8f211 (feat: Added new friendship feature)
 }
 
 export default ChatBase;
